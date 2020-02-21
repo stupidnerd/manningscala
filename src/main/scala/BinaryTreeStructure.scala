6bdf956382962f15057361b572ba573dfa73224b
Evgeniy Kurpanov <e.kurpanov@b-monitor.ru>
2020-02-21 15:14:35 +0300

    [+] added tree maximum, depth and map impl, size impl changed

--- src/main/scala/BinaryTreeStructure.scala
+++ src/main/scala/BinaryTreeStructure.scala
@@ -4,14 +4,25 @@ object BinaryTreeStructure {
 
   case class Leaf[A](value: A) extends Tree[A]
 
-  case class Branch[A](lead: Tree[A], right: Tree[A]) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
-  def size[A](tree: Tree[A]): Int = {
-    def loop[B](t: Tree[B], acc: Int): Int = t match {
-      case Leaf(_) => 1
-      case Branch(lead, right) => 1 + loop(lead, acc) + loop(right, acc)
-    }
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
 
-    loop(tree, 0)
+  def maximum(tree: Tree[Int]): Int = tree match {
+    case Leaf(x) => x
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+  def depth[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
+
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Leaf(x) => Leaf(f(x))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,8 +7,11 @@ import BinaryTreeStructure.Leaf
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val myTree = Branch(Branch(Leaf(2), Leaf(3)), Branch(Branch(Leaf(2), Leaf(3)), Leaf(4)))
+    val myTree = Branch(Branch(Leaf(2), Leaf(3)), Branch(Branch(Leaf(10), Leaf(3)), Leaf(4)))
     println(myTree)
     println(BinaryTreeStructure.size(myTree))
+    println(BinaryTreeStructure.maximum(myTree))
+    println(BinaryTreeStructure.depth(myTree))
+    println(BinaryTreeStructure.map(myTree)(_ * 2))
   }
 }
